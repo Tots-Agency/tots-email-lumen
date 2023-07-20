b8923462c6b67257463489c2bcfc3c8aaa400433
Matias Camiletti <matias.camiletti@gmail.com>
2023-07-20 17:07:29 -0300

    Add provider.

---
+++ src/Providers/EmailProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tots\Email\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class EmailProvider extends ServiceProvider
+{
+    /**
+     * Boot the blog services for the application.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        
+    }
+}
